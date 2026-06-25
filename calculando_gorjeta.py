c133793bfb3ec2b3fe2386a036698b73956a51b9
bgabrielcode <user19384839@gmail.com>
2026-06-25 17:27:12 -0300

    gerador_senha

--- calculando_gorjeta.py
+++
@@ -1,6 +0,0 @@
-valor_da_conta = float(input("Digite o valor da conta: "))
-porcentagem_da_gorjeta = float(input("Digite a porcetagem da gorjeta: "))
-gorjeta = (porcentagem_da_gorjeta / 100) * valor_da_conta
-total_a_pagar = valor_da_conta + gorjeta
-print(f"Valor da gorjeta {gorjeta:.2f}")
-print(f"Total a pagar {total_a_pagar:.2f}")
