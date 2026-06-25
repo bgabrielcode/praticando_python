ce1bf5db407d57b3f401e27dce9f88d0b64b2604
bgabrielcode <user19384839@gmail.com>
2026-06-25 17:53:44 -0300

    Adicionando novos projetos em Python

---
+++ gerenciador_tarefas.py
@@ -0,0 +1,8 @@
+def gerenciador_tarefas():
+    tarefas = []
+
+    while True:
+        print("\n1. Adicionar tarefa")
+        print("2. Visualizar tarefa")
+        print("3. Remover tarefa")
+        print("4. Sair")
